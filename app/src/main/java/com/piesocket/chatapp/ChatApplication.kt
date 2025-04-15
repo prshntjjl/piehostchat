f24e0a69878afb4e04ccae3e445b275131b9735c
Prashant Jajal <prashantjajal81@gmail.com>
2025-04-15 23:45:19 +0530

    removed unused code

--- app/src/main/java/com/piesocket/chatapp/ChatApplication.kt
+++ app/src/main/java/com/piesocket/chatapp/ChatApplication.kt
@@ -8,20 +8,4 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
 @HiltAndroidApp
-class ChatApplication : Application() {
-    
-    private val applicationScope = CoroutineScope(SupervisorJob())
-    
-    val database by lazy { AppDatabase.getDatabase(this, applicationScope) }
-    val webSocketManager by lazy { WebSocketManager(this) }
-    
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-    }
-    
-    companion object {
-        lateinit var instance: ChatApplication
-            private set
-    }
-} 
+class ChatApplication : Application()
